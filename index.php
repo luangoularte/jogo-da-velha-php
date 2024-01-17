8b9e5ca1a6070e287683d511f622a145e855a248
luangoularte <luan.ag2012@hotmail.com>
2024-01-17 20:10:15 -0300

    att

--- index.php
+++ index.php
@@ -19,12 +19,14 @@ do {
 
             Posiçôes: | Tabuleiro
                       |     
-              0|1|2   |     $board[0]|$board [1]|$board[2] 
-              3|4|5   |     $board[3]|$board [4]|$board[5]
-              6|7|8   |     $board[6]|$board [7]|$board[8]
+              0|1|2   |     $board[0]|{$board [1]}|$board[2] 
+              3|4|5   |     $board[3]|{$board [4]}|$board[5]
+              6|7|8   |     $board[6]|{$board [7]}|$board[8]
             
             EOL
         ;
+        
+        echo "\n";
 
         $position = (int) readline("Player {$player}, digite a sua posição: ");
 
@@ -83,12 +85,14 @@ do {
 
             Posiçôes: | Tabuleiro
                       |     
-              0|1|2   |     $board[0]|$board [1]|$board[2] 
-              3|4|5   |     $board[3]|$board [4]|$board[5]
-              6|7|8   |     $board[6]|$board [7]|$board[8]
+              0|1|2   |     $board[0]|{$board [1]}|$board[2] 
+              3|4|5   |     $board[3]|{$board [4]}|$board[5]
+              6|7|8   |     $board[6]|{$board [7]}|$board[8]
             
             EOL
     ;
+
+    echo "\n";
 
     if ($winner === "X") {
         echo "VENCEDOR: {$playerOne}.\n";
